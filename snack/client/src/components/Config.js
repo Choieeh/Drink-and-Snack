21c5e293098085fb93d1e94afba5ebfe4519f9b1
Choieeh <chyh0182@naver.com>
2021-03-08 13:33:18 +0000

    boiler plate & api

---
+++ snack/client/src/components/Config.js
@@ -0,0 +1,4 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/';
---
+++ snack/server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://Ehyun:asdf1234@cluster0.yqvbz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
